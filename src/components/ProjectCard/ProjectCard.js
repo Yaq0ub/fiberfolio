1b17bbb2119d2dce50596fc6c20a515e71dad6be
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2023-04-24 08:48:31 -0600

    setup project folder structure

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const ProjectCard = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default ProjectCard
