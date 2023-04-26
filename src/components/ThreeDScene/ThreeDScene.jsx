36bdda52c03bd7305087b753ea6eae7a06a91484
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2023-04-26 15:37:19 -0600

    Added custom environment and effect to scene

--- src/components/ThreeDScene/ThreeDScene.jsx
+++ src/components/ThreeDScene/ThreeDScene.jsx
@@ -1,12 +1,13 @@
 // Import the required libraries
 import React from 'react'
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-import Rystal from './Rystal/Rystal';
+import Rystal  from './scene/Rystal/Rystal';
+import CustomEnvironment from './scene/CustomEnvironment';
+import Effects from './scene/Effects'
 
 import './ThreeDScene.module.css'
-
-import { OrbitControls } from '@react-three/drei';
 
 // Define constants for camera settings and model paths
 const CONSTANTS = {
@@ -14,7 +15,8 @@ const CONSTANTS = {
   glcontext: { logarithmicDepthBuffer: true, antialias: false },
   dpr: [1, 1.5],
   hemisphereLight: {color: "white", groundColor: "blue", intensity: 0.75},
-  spotlight: { position : [50, 50, 10], angle: 0.9, penumbra: 1 },
+  spotlight: { position : [30, 50, 10], angle: 0.9, penumbra: 0.5 },
+  rystal: { position: [0,0,0] }
 };
 
 
@@ -39,9 +41,15 @@ const ThreeDScene = () => {
       <spotLight {...CONSTANTS.spotlight} />
 
       {/* Add the model to the scene */}
-      <Rystal position={[0,0,0]} />
+      <Rystal {...CONSTANTS.rystal} />
 
-      {/*Add Orbital Controls to the scene */}
+      {/* Add the custom Environment containing Light former */}
+      <CustomEnvironment />
+      
+      {/* Add Effects object containing leva controls for LUTs and other effects */}
+      <Effects />
+
+      {/* Add Orbital Controls to the scene */}
       <OrbitControls />
       
       </Canvas>
