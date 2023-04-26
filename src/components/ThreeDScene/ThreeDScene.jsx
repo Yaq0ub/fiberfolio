44401e17c4b772295c1650dec00e8269e071a6df
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2023-04-25 18:50:34 -0600

    fix

--- src/components/ThreeDScene/ThreeDScene.jsx
+++ src/components/ThreeDScene/ThreeDScene.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Canvas from '@react-three/fiber';
+import React from 'react'
 
 const ThreeDScene = () => {
   return (
-    <>
-    </>
+    <div>
+      
+    </div>
   )
 }
 
