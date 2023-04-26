7a32596383ab018c40119cbcc6e59624e9bd2559
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2023-04-26 16:43:35 -0600

    Added ability to flip model

--- src/components/ThreeDScene/scene/Models/BasicModel.jsx
+++ src/components/ThreeDScene/scene/Models/BasicModel.jsx
@@ -2,12 +2,23 @@
 import { useGLTF } from "@react-three/drei";
 
 // Define the BasicModel component
-function BasicModel({ url, ...props }) {
+function BasicModel({ url, flipX = false, flipY = false, flipZ = false, ...props }) {
     // Use the useGLTF hook to load the 3D model from the provided URL
     const { scene } = useGLTF(url);
-
+    
+    // Calculate the scale values based on the flip flags
+    const scaleX = flipX ? -1 : 1;
+    const scaleY = flipY ? -1 : 1;
+    const scaleZ = flipZ ? -1 : 1;
+    
     // Return a primitive object with the loaded scene and any additional props passed to the component
-    return <primitive object={scene} {...props} />;
+    return (
+        <primitive
+            object={scene}
+            scale={[scaleX, scaleY, scaleZ]}
+            {...props}
+        />
+    )
 }
 
 // Export the BasicModel component for use in other parts of the application
