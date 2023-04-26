0ed3336e27b59aa9e8b75c991a5f372f30157abe
Yaq0ub <122842387+Yaq0ub@users.noreply.github.com>
2023-04-25 22:37:40 -0600

    Add crytal to the 3d scene

--- src/components/ThreeDScene/ThreeDScene.jsx
+++ src/components/ThreeDScene/ThreeDScene.jsx
@@ -1,22 +1,36 @@
+// Import the required libraries
 import React from 'react'
 import { Canvas } from '@react-three/fiber';
 
+import Rystal from './Rystal/Rystal';
+
+import './ThreeDScene.module.css'
+
+// Define constants for camera settings and model paths
 const CONSTANTS = {
   camera: { position: [10, 10, 16], fov: 100 },
 };
 
+
+
+// Define the ThreeDScene component
 const ThreeDScene = () => {
   return (
     <>
+      {/* Render the Canvas component with custom camera and WebGL settings */}
       <Canvas 
+        // Set the camera properties
         camera = {CONSTANTS.camera}
+        // Set the WebGL context properties
         gl = {{ logarithmicDepthBuffer: true, antialias: false }}
+        // Set the device pixel ratio
         dpr = {[1, 1.5]}
       >
-
+      <Rystal position={[0,0,0]} />
       </Canvas>
     </>
   )
 }
 
+// Export the ThreeDScene component
 export default ThreeDScene
